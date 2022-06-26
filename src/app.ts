2fdca7cf4d08675d537e35faa51d1efcd44bd066
KazhymukhanYerkinov <kazhymukhanyerkinov@gmail.com>
2022-06-26 21:22:44 +0600

    user.controller.ts added

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,25 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
 import { LoggerService } from './logger/logger.service';
+import { userRouter } from './users/users';
+import { UserController } from './users/users.controller';
 
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
 	logger: LoggerService;
+	userController: UserController;
 
-	constructor(logger: LoggerService) {
+	constructor(logger: LoggerService, userController: UserController) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
+		this.userController = userController;
 	}	
 
 	useRoutes() {
-		
+		this.app.use('/users', this.userController.router);
 	}
 
 	public async init() {
--- src/users/users.ts
+++ src/users/users.ts
@@ -1,1 +1,18 @@
-export {};
+import express from 'express';
+
+const userRouter = express.Router();
+
+userRouter.use((req, res, next) => {
+	console.log('Обработчик users');
+	next();
+});
+
+userRouter.post('/login', (req, res) => {
+	res.send('login');
+});
+
+userRouter.post('/register', (req, res) => {
+	res.send('register');
+});
+
+export { userRouter };
