18363142a0b0e7ec8f4d6e1f84522555ceff353f
KazhymukhanYerkinov <kazhymukhanyerkinov@gmail.com>
2022-06-29 22:34:35 +0600

    change mange

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,9 +4,10 @@ import { BaseController } from "../common/base.controller";
 import { HTTPError } from "../errors/http-error.class";
 import { ILogger } from "../logger/logger.interface";
 import { TYPES } from "../types";
+import { IUserController } from "./users.controller.interface";
 
 @injectable()
-export class UserController extends BaseController {
+export class UserController extends BaseController implements IUserController {
 
 	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
 		super(loggerService);
